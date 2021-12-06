892c5ff57c845adc9b397e0761c52d5a79f5b845
Rashid Boyko <rashid.boyko@codurance.com>
2021-12-06 21:11:55 +0000

    Corrected URL output for Opensearch dashboard

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  opensearch_endpoint      = module.opensearch.opensearch_endpoint
+  opensearch_endpoint      = "https://${module.opensearch.opensearch_endpoint}/_dashboards/"
   opensearch_arn           = module.opensearch.opensearch_arn
   access_policies          = data.aws_iam_policy_document.es_access_policy.json
   cognito_user_pool_id     = lookup(module.cognito.cognito_map, "user_pool")
